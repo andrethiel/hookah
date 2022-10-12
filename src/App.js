39426916993c78df61fb7e3f41e8859f22275f3e
Andre Luiz Thiel <43017985+andrethiel@users.noreply.github.com>
2022-10-12 15:20:23 -0300

    inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Container,
+  SimpleGrid,
+  Flex,
+  Heading,
+  Text,
+  Box,
+  Button,
+  Center,
+} from "@chakra-ui/react";
+import { GrInstagram } from "react-icons/gr";
+import back from "./back.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box
+      position={"relative"}
+      bgImage={back}
+      backgroundRepeat="no-repeat"
+      minH={"100vh"}
+      bgSize={"cover"}
+    >
+      <Container
+        as={SimpleGrid}
+        maxW={"7xl"}
+        columns={{ base: 1, md: 2 }}
+        spacing={{ base: 8, md: 14 }}
+        py={{ base: 20, md: 36 }}
+      >
+        <Flex flex={1} align={"center"} justify={"center"}></Flex>
+        <Flex
+          flex={1}
+          align={"center"}
+          justify={"center"}
+          textAlign={"center"}
+          direction={"column"}
+          spacing={{ base: 8, md: 14 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Heading
+            fontSize={{ base: "6xl", sm: "4xl", md: "8xl" }}
+            lineHeight={"110%"}
+            color={"white"}
+            fontFamily={"League Gothic"}
+          >
+            PREMIUM <br />
+            <Text
+              as={"span"}
+              color={"white"}
+              fontSize={{ base: "6xl", sm: "4xl", md: "8xl" }}
+            >
+              HOME
+              <br />
+            </Text>
+            <Text
+              as={"span"}
+              color={"white"}
+              fontSize={{ base: "4xl", sm: "4xl", md: "8xl" }}
+            >
+              HOOKAH
+            </Text>
+          </Heading>
+          <Text
+            as={"span"}
+            color={"whiteAlpha.700"}
+            fontSize={{ base: "3xl", sm: "4xl", md: "3xl" }}
+          >
+            Um novo conceito de essências está chegando em Campo Largo
+          </Text>
+          <Text
+            as={"span"}
+            color={"whiteAlpha.600"}
+            fontSize={{ base: "2xl", sm: "4xl", md: "2xl" }}
+            marginBottom={"10"}
+          >
+            Visite nosso perfil e fique por dentro de todas as novidades
+          </Text>
+          <Button
+            w={"full"}
+            maxW={"md"}
+            leftIcon={<GrInstagram />}
+            _hover={{ bg: "#ea0c5f" }}
+            bgGradient="linear(to-r, #002296, #82008f, #c0007a, #ea0c5f, #ff5341, #ff8820, #f6ba00)"
+            onClick={() =>
+              (window.location.href =
+                "https://www.instagram.com/_premium.home/")
+            }
+          >
+            <Center>
+              <Text>Visitar</Text>
+            </Center>
+          </Button>
+        </Flex>
+      </Container>
+    </Box>
   );
 }
 
