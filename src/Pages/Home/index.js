cc65c9798d5591bc0c47f575b989fd2bca1cc55d
Andre Luiz Thiel <43017985+andrethiel@users.noreply.github.com>
2022-10-19 14:59:38 -0300

    lova lgo

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,94 @@
+import {
+  Container,
+  SimpleGrid,
+  Flex,
+  Heading,
+  Text,
+  Box,
+  Button,
+  Center,
+  Stack,
+  Image,
+} from "@chakra-ui/react";
+import { GrInstagram } from "react-icons/gr";
+import { BsShop } from "react-icons/bs";
+import back from "../../back.jpg";
+import logo from "../../branca-sem-fundo.png";
+
+function Home() {
+  return (
+    <Box
+      position={"relative"}
+      bgImage={back}
+      backgroundRepeat="no-repeat"
+      minH={"100vh"}
+      bgSize={"cover"}
+    >
+      <Container as={SimpleGrid} maxW={"7xl"} columns={{ base: 1, md: 2 }}>
+        <Flex flex={1} align={"center"} justify={"center"}></Flex>
+        <Flex
+          flex={1}
+          align={"center"}
+          justify={"center"}
+          textAlign={"center"}
+          direction={"column"}
+          spacing={{ base: 8, md: 14 }}
+        >
+          <Image src={logo} width={"70%"} />
+          <Text
+            as={"span"}
+            color={"whiteAlpha.700"}
+            fontSize={{ base: "3xl", sm: "4xl", md: "3xl" }}
+          >
+            Um novo conceito de essências está chegando em Campo Largo
+          </Text>
+          <Text
+            as={"span"}
+            color={"whiteAlpha.600"}
+            fontSize={{ base: "2xl", sm: "4xl", md: "2xl" }}
+            marginBottom={"10"}
+          >
+            Visite nosso perfil e fique por dentro de todas as novidades
+          </Text>
+          <Stack
+            width={"100%"}
+            spacing={3}
+            align={"center"}
+            justify={"center"}
+            textAlign={"center"}
+          >
+            <Button
+              w={"full"}
+              maxW={"md"}
+              leftIcon={<GrInstagram />}
+              _hover={{ bg: "#ea0c5f" }}
+              bgGradient="linear(to-r, #002296, #82008f, #c0007a, #ea0c5f, #ff5341, #ff8820, #f6ba00)"
+              onClick={() =>
+                (window.location.href =
+                  "https://www.instagram.com/_premium.home/")
+              }
+            >
+              <Center>
+                <Text>Visitar</Text>
+              </Center>
+            </Button>
+            <Button
+              w={"full"}
+              maxW={"md"}
+              leftIcon={<BsShop />}
+              onClick={() =>
+                (window.location.href = "https://premiumhomehookah.kyte.site/")
+              }
+            >
+              <Center>
+                <Text>Loja Online</Text>
+              </Center>
+            </Button>
+          </Stack>
+        </Flex>
+      </Container>
+    </Box>
+  );
+}
+
+export default Home;
